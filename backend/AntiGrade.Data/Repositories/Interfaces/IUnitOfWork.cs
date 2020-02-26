862a8e5e981326b38812d4d6568b5ebd9ff9a4ee
Georgy Teslenko <teslenko.digitalskynet@gmail.com>
2020-02-26 13:06:38 +0300

    added repository

---
+++ backend/AntiGrade.Data/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+using System.Threading.Tasks;
+using AntiGrade.Data.Repositories.Interfaces;
+using AntiGrade.Shared.Models;
+using AntiGrade.Shared.Models.Identity;
+
+namespace AntiGrade.Data.Repositories.Interfaces
+{
+    public interface IUnitOfWork
+    {
+
+        IRepository<TEntity, TId> GetRepository<TEntity, TId>() where TEntity : class, IEntity<TId>;
+
+        IRepository<User, int> UserRepository
+        {
+            get;
+        }
+        
+        Task<int> Save();
+    }
+}
