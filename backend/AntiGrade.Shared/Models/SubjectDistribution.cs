0de976662cb3bb9d82e8657dfef277d159429d40
Georgy Teslenko <teslenko.digitalskynet@gmail.com>
2020-03-05 17:34:54 +0300

    ids added

--- backend/AntiGrade.Shared/Models/SubjectDistribution.cs
+++ backend/AntiGrade.Shared/Models/SubjectDistribution.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using AntiGrade.Shared.Enums;
 
 namespace AntiGrade.Shared.Models
@@ -6,6 +7,15 @@ namespace AntiGrade.Shared.Models
     public class SubjectDistribution : IEntityWithId<int>
     {
         public int Id { get; set; }
+
+        [ForeignKey(nameof(Group))]
+        public int GroupId {get;set;}
+
+        [ForeignKey(nameof(Subject))]
+        public int SubjectId {get;set;}
+
+        [ForeignKey(nameof(Teacher))]
+        public int TeacherId {get;set;}
         public Group Group {get;set;}
         public Subject Subject {get;set;}
         public Employee Teacher {get;set;}
